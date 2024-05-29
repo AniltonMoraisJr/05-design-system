7a13193f8b6bafe3466ede63bf4f6d1ae8586e9e
AniltonFMoraisJr <anilton.morais@bovexo.com>
2024-05-29 14:06:20 -0300

    feat: implementing component textinput

--- packages/docs/src/stories/TextInput.stories.tsx
+++ packages/docs/src/stories/TextInput.stories.tsx
@@ -1,18 +1,42 @@
-import { TextInput, TextInputProps } from '@aniltonjr-ignite-ui/react'
+import {
+  Box,
+  Text,
+  TextInput,
+  TextInputProps,
+} from '@aniltonjr-ignite-ui/react'
 import type { StoryObj, Meta } from '@storybook/react'
 
 export default {
-  title: 'Typography/TextInput',
+  title: 'Form/TextInput',
   component: TextInput,
-  args: {
-    children: 'Example test',
-  },
+  args: {},
+  decorators: [
+    (Story) => {
+      return (
+        <Box
+          as="label"
+          css={{ display: 'flex', flexDirection: 'column', gap: '$2' }}
+        >
+          <Text size="sm">Email address</Text>
+          {Story()}
+        </Box>
+      )
+    },
+  ],
 } as Meta
 
-export const Primary: StoryObj<TextInputProps> = {}
-export const CustomTag: StoryObj<TextInputProps> = {
+export const Primary: StoryObj<TextInputProps> = {
   args: {
-    children: 'Strong textInput',
-    as: 'strong',
+    placeholder: 'Type your name',
+  },
+}
+export const Disabled: StoryObj<TextInputProps> = {
+  args: {
+    disabled: true,
+  },
+}
+export const WithPrefix: StoryObj<TextInputProps> = {
+  args: {
+    prefix: 'call.com/',
   },
 }
--- packages/react/src/components/TextInput/index.tsx
+++ packages/react/src/components/TextInput/index.tsx
@@ -1,10 +1,20 @@
-import React from 'react'
-import { TextInputContainer } from './styles'
+import React, { ComponentProps } from 'react'
+import { Prefix, TextInputContainer, Input } from './styles'
 
 // import { Container } from './styles';
 
-export interface TextInputProps {}
+export interface TextInputProps extends ComponentProps<typeof Input> {
+  prefix?: string
+}
 
-export const TextInput: React.FC = () => {
-  return <TextInputContainer></TextInputContainer>
+export const TextInput: React.FC<TextInputProps> = ({
+  prefix,
+  ...inputProps
+}) => {
+  return (
+    <TextInputContainer>
+      {!!prefix && <Prefix>{prefix}</Prefix>}
+      <Input {...inputProps} />
+    </TextInputContainer>
+  )
 }
